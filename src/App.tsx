f7933644120ca54aa61272dde7773bb73be86831
Willian Dias Tamagi <will.tamagi@gmail.com>
2021-10-30 15:44:41 -0300

    Add react router and initial structure to the pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { ApolloProvider } from "@apollo/client";
 import { ThemeProvider } from "@emotion/react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 import GlobalStyles from "./GlobalStyles";
 import theme from "./theme";
 import client from "./graphql/client";
+
+const Main = React.lazy(() => import("./modules/Main"));
+const Detail = React.lazy(() => import("./modules/Detail"));
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <ApolloProvider client={client}>
         <GlobalStyles />
-        <div>Hello There!</div>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Router>
+            <Switch>
+              <Route exact path="/detail/:id">
+                <Detail />
+              </Route>
+              <Route exact path="/">
+                <Main />
+              </Route>
+              <Route path="/*">
+                <Redirect to="/" />
+              </Route>
+            </Switch>
+          </Router>
+        </Suspense>
       </ApolloProvider>
     </ThemeProvider>
   );
